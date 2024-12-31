c634f70c93ef592bbe67915c6c0ca114a761fbd6
ahmed.sadek <ahmed.sadek@acmesaico.com>
2025-01-01 00:12:29 +0200

    add search for news

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -4,8 +4,29 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\News;
+use App\Services\NewsService;
+use Illuminate\Http\Response;
 
 class NewsController extends Controller
 {
-    
+    public function searchFormData()
+    {
+        $response = NewsService::getLookups();
+        return response()->json([
+            'status' => Response::HTTP_OK,
+            'data' => $response
+        ]);
+    }
+
+
+    public function serach(Request $request)
+    {
+        $news = NewsService::search($request);
+        return response()->json([
+            'status' => Response::HTTP_OK,
+            'data' => $news
+        ]);
+    }
+
 }
--- app/Services/NewsService.php
+++ app/Services/NewsService.php
@@ -10,4 +10,29 @@ use Illuminate\Support\Facades\Log;
 class NewsService
 {
 
+    public static function getLookups()
+    {
+        $categories = News::distinct()->pluck('category');
+        $sources = News::distinct()->pluck('source');
+        return ([
+            'categories' => $categories,
+            'sources' => $sources,
+        ]);
+    }
+
+    public static function search($request)
+    {
+        $query = News::query();
+        if ($request->has('keyword')) {
+            $query->where('title', 'like', '%' . $request->keyword . '%');
+        }
+        if ($request->has('category') && !empty($request->category)) {
+            $query->where('category', $request->category);
+        }
+        if ($request->has('source') && !empty($request->source)) {
+            $query->where('source', $request->source);
+        }
+        $news = $query->orderBY('published_at', 'desc')->get();
+        return  $news;
+    }
 }
